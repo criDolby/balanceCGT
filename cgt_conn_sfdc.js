0e0cf982631401aef65f07bc69f71ca3f9005dbc
criDolby <166417409+criDolby@users.noreply.github.com>
2024-04-17 10:21:27 +0200

    Update cgt_conn_sfdc.js

--- cgt_conn_sfdc.js
+++ cgt_conn_sfdc.js
@@ -1,9 +1,4 @@
-const commUrl = 'https://cgtspa--devmerge.sandbox.my.site.com/CGTPortaleRegistrazioneClienti';
-const redirectURI = 'https://cridolby.github.io/prima/';
-const azureLogoutURI = 'https://cgtb2cstaging.b2clogin.com/cgtb2cstaging.onmicrosoft.com/b2c_1a_sfidentity_signup_signin_v1/oauth2/v2.0/logout';
-const clientId = '3MVG9bmlmX4LX1ZvlQPwDwh5N1fOBV4wLtXixm2uPy7urVdUASmTjYk.YlQ65RUOIlT8k8OVhqIRROaa2B9yf';
-const ssoProvider = 'AzureADB2CTest';
-
+import  {commUrl, redirectURI, azureLogoutURI, clientId, ssoProvider} from './cgt_conn_sfdc_const.js';
 
 async function initiateSSOFlow() {
 //-- Costanti & Variabili --//
