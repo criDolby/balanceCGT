5651e996597a43bb554b95ccfa6d979dabe8ddd8
CristianM <c.mele@balanceconsulting.it>
2024-04-21 16:11:41 +0200

    add file system structure

--- cgt_conn_sfdc_const_s.js
+++ cgt_conn_sfdc_const_s.js
@@ -12,6 +12,16 @@
 
 
                     Created By Balance Spa
+                    cgt_conn_sfdc_const_s
+                    versione 1.1
+                    18/04/2024
 **********************************************************************************************/
 
-Snippet Per integrare SF con le varie piattaforme CGT
+
+const commUrl = 'https://cgtspa--devmerge.sandbox.my.site.com/CGTPortaleRegistrazioneClienti';
+const redirectURI = 'https://cridolby.github.io/prima/';
+const azureLogoutURI = 'https://cgtb2cstaging.b2clogin.com/cgtb2cstaging.onmicrosoft.com/b2c_1a_sfidentity_signup_signin_v1/oauth2/v2.0/logout';
+const clientId = '3MVG9bmlmX4LX1ZvlQPwDwh5N1fOBV4wLtXixm2uPy7urVdUASmTjYk.YlQ65RUOIlT8k8OVhqIRROaa2B9yf';
+const ssoProvider = 'AzureADB2CTest';
+const sorgente = 'sitoCGT';
+
