99c2dff87b7e606835cacb96bb1323c54ec1f147
CristianM <c.mele@balanceconsulting.it>
2024-04-22 14:28:08 +0200

    update costanti

--- Snippet_PORTALE/cgt_conn_sfdc_const_p.js
+++ Snippet_PORTALE/cgt_conn_sfdc_const_p.js
@@ -18,7 +18,7 @@
 **********************************************************************************************/
 
 const commUrl = 'https://cgtspa--devmerge.sandbox.my.site.com/CGTPortaleRegistrazioneClienti'; // Service Provider
-const redirectURI = 'https://cridolby.github.io/testPortaleCGT/Snippet_PORTALE'; // Redirect 
+const redirectURI = 'https://cridolby.github.io/balanceCGT/Snippet_PORTALE'; // Redirect 
 const azureLogoutURI = 'https://cgtb2cstaging.b2clogin.com/cgtb2cstaging.onmicrosoft.com/b2c_1a_sfidentity_signup_signin_v1/oauth2/v2.0/logout'; // Endpoint Logout Azure
 const clientId = '3MVG9bmlmX4LX1ZvlQPwDwh5N1fOBV4wLtXixm2uPy7urVdUASmTjYk.YlQ65RUOIlT8k8OVhqIRROaa2B9yf';	// Client ID Connected App
 const ssoProvider = 'AzureADB2CTest'; //Auth Provider 
