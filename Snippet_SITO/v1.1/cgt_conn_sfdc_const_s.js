d5fd8af40f5094d5a25e797e427961f7a5493b91
CristianM <c.mele@balanceconsulting.it>
2024-04-22 12:16:16 +0200

    Update File System

---
+++ Snippet_SITO/v1.1/cgt_conn_sfdc_const_s.js
@@ -0,0 +1,25 @@
+/**************************************************************************************                                                                             
+                                                                             
+                                                                                                                                                       
+           _                                                    __    _      
+          | |                                                  / _|  | |     
+  ___ __ _| |_             ___ ___  _ __  _ __             ___| |_ __| | ___ 
+ / __/ _` | __|           / __/ _ \| '_ \| '_ \           / __|  _/ _` |/ __|
+| (_| (_| | |_           | (_| (_) | | | | | | |          \__ \ || (_| | (__ 
+ \___\__, |\__|           \___\___/|_| |_|_| |_|          |___/_| \__,_|\___|
+      __/ |      ______                           ______                     
+     |___/      |______|                         |______|                    
+
+
+                    Created By Balance Spa
+                    cgt_conn_sfdc_const_s
+                    versione 1.1
+                    18/04/2024
+**********************************************************************************************/
+
+const commUrl = 'https://cgtspa--devmerge.sandbox.my.site.com/CGTPortaleRegistrazioneClienti'; // Service Provider
+const redirectURI = 'https://sitoEsempio.it/'; // Redirect 
+const azureLogoutURI = 'https://cgtb2cstaging.b2clogin.com/cgtb2cstaging.onmicrosoft.com/b2c_1a_sfidentity_signup_signin_v1/oauth2/v2.0/logout'; // Endpoint Logout Azure
+const clientId = '3MVG9bmlmX4LX1ZvlQPwDwh5N1fOBV4wLtXixm2uPy7urVdUASmTjYk.YlQ65RUOIlT8k8OVhqIRROaa2B9yf';	// Client ID Connected App
+const ssoProvider = 'AzureADB2CTest'; //Auth Provider 
+const sorgente = 'sitoCGT'; 
