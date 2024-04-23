60a245b61038fe775a216638ca92ecdba92ef320
CristianM <c.mele@balanceconsulting.it>
2024-04-23 14:13:43 +0200

    test per nuova implementazione

--- cgt_conn_sfdc_s.js
+++ cgt_conn_sfdc_s.js
@@ -19,11 +19,13 @@
 
 async function initiateSSOFlow() {
 
+    document.cookie = "sorgenteUser=" + sorgente + ";path=/;Secure;SameSite=None";
     document.cookie = "sorgenteUser=" + sorgente + ";domain=.my.site.com;path=/;Secure;SameSite=None";
 
 //-- PCKE Generator --//
 
     let codeVerifier = generateRandomString();
+    document.cookie = "codeVerifier=" + sorgente + ";domain=.my.site.com;path=/;Secure;SameSite=None";
     localStorage.setItem("pkce_code_verifier", codeVerifier);
         // Hash and base64-urlencode the secret to use as the challenge
     let codeChallenge = await pkceChallengeFromVerifier(codeVerifier);
