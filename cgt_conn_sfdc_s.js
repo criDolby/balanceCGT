4461d2e9a89cb73f3548e2a2b0d6ceb4f1ca1f2a
CristianM <c.mele@balanceconsulting.it>
2024-05-02 10:22:50 +0200

    update logout function

--- cgt_conn_sfdc_s.js
+++ cgt_conn_sfdc_s.js
@@ -117,8 +117,8 @@ function getUserInfo(accessToken) {
 }
 
 function logoutUser() {
-    let redirectLogoutURL = azureLogoutURI + '?post_logout_redirect_uri=' + redirectURI;
-   //let redirectLogoutURL = azureLogoutURI + '?post_logout_redirect_uri=' + commUrl + '/s/logout?redirectUrl=' +redirectURI;
+   // let redirectLogoutURL = azureLogoutURI + '?post_logout_redirect_uri=' + redirectURI;
+    let redirectLogoutURL = azureLogoutURI + '?post_logout_redirect_uri=' + commUrl + '/secur/logout.jsp';
     let revokeTokenURI = '/services/oauth2/revoke';
 
     let accessToken = getCookie("SFToken");
@@ -141,8 +141,9 @@ function logoutUser() {
                 document.cookie = "inst=; expires=Thu, 01 Jan 1970 00:00:00 UTC;  path=/";
                 //window.location = commUrl + complProfilo +'?logout=true' + '&redirectURL=' +redirectLogoutURL ;
 */
-                window.location = commUrl + '/secur/logout.jsp';
-               // window.location = redirectLogoutURL;
+     //           window.location = commUrl + '/secur/logout.jsp';
+
+                window.location = redirectLogoutURL;
             } else {
                 window.location = redirectLogoutURL;
                 //onError("An Error Occured during Forgot Password Step: " +
