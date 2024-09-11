183e46ee05bc235d9b0da76b5ab89d3575846e69
CristianM <c.mele@balanceconsulting.it>
2024-09-11 18:14:51 +0200

    versione 1.5

---
+++ RELEASE/V1.5/cgt_conn_sfdc_const.js
@@ -0,0 +1,26 @@
+/**************************************************************************************                                                                             
+                                                                             
+                                                                                                                                                       
+           _                                                    __    _      
+          | |                                                  / _|  | |     
+  ___ __ _| |_             ___ ___  _ __  _ __             ___| |_ __| | ___ 
+ / __/ _` | __|           / __/ _ \| '_ \| '_ \           / __|  _/ _` |/ __|
+| (_| (_| | |_           | (_| (_) | | | | | | |          \__ \ || (_| | (__ 
+ \___\__, |\__|           \___\___/|_| |_|_| |_|          |___/_| \__,_|\___|
+      __/ |      ______                           ______                     
+     |___/      |______|                         |______|                    
+
+
+                    Created By Balance Spa
+                    cgt_conn_sfdc_const
+                    versione 1.
+                    25/07/2024
+**********************************************************************************************/
+
+const commUrl = 'https://cgtspa--uat.sandbox.my.site.com/CGTPortaleRegistrazioneClienti'; // Service Provider
+const redirectURI = 'https://sitoEsempio.it/'; // Redirect Sito/Portale/Eventi
+const azureLogoutURI = 'https://cgtb2cstaging.b2clogin.com/cgtb2cstaging.onmicrosoft.com/b2c_1a_sfidentity_signup_signin_v1/oauth2/v2.0/logout'; // Endpoint Logout Azure
+const clientId = '3MVG9bmlmX4LX1ZuAYAJHiS3dpHiJeBZ6NnK6mLmJiUy_SqlDcK34vfbGcrj0.6yJVoqNfjugFgmnRvfh.OII';	// Client ID Connected App
+const ssoProvider = 'AzureADB2CTest'; //Auth Provider 
+const sorgente = 'portaleCGT'; // scegliere tra portaleCGT, sitoCGT, eventiCGT
+
