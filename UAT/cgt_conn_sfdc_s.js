10eab1c7b7629f7ac8d46209e0aa1b98cb5d5d20
CristianM <c.mele@balanceconsulting.it>
2024-11-05 14:01:57 +0100

    dsgdfgdfg

--- UAT/cgt_conn_sfdc_s.js
+++ UAT/cgt_conn_sfdc_s.js
@@ -28,13 +28,13 @@ async function initiateSSOFlow() {
     let responsType = 'code';
 
 //-- Costruzione redirect --//
-    let redirectURL = 'https://cgtspa--uat.sandbox.my.site.com/CGTPortaleRegistrazioneClienti/services/oauth2/authorize' +
+    let redirectURL = 'https://cgtspa--uat.sandbox.my.site.com/CGTPortaleRegistrazioneClienti/services/auth/oauth/AzureADB2CTest' +
      //'?client_id=' + clientId + 
-     '&prompt=login%20consent' +
+    // '&prompt=login%20consent' +
     // '&redirect_uri=' + redirectURI + 
     //'&redirect_uri=' + 'https://cgtspa--uat.sandbox.my.site.com/CGTPortaleRegistrazioneClienti/services/authcallback/AzureADB2CTest' + 
     // '&response_type=' + responsType +
-     '&sso_provider=' + ssoProvider + 
+    // '&sso_provider=' + ssoProvider + 
     // '&code_challenge=' + encodeURIComponent(codeChallenge) + 
     // '&code_verifier=' + encodeURIComponent(codeVerifier) + 
     // '&ui_locales=en_US';
