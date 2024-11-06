9e2d1c42e2a2af0acfe03f9e514d7df67b3a2a70
CristianM <c.mele@balanceconsulting.it>
2024-11-06 16:10:26 +0100

    asdasda

--- cgt_conn_sfdc_s.js
+++ cgt_conn_sfdc_s.js
@@ -41,8 +41,8 @@ async function initiateSSOFlow() {
 
 
      let redirectURL = 'https://cgtspa--devmerge.sandbox.my.salesforce.com/services/auth/sso/AzureADB2CTest' +
-     '/?ui_locales=' + 'en' +
-     '&state=ciao';
+     '/?ui_locales=' + 'en' ;
+     //'&state=ciao';
      
     // let redirectURL = 'https://cgtb2cstaging.b2clogin.com/cgtb2cstaging.onmicrosoft.com/b2c_1a_sfidentity_signup_signin_v2/oauth2/v2.0/authorize?client_id=a6c3a0eb-7386-402e-8291-b1eed2105ba4&scope=a6c3a0eb-7386-402e-8291-b1eed2105ba4+openid&response_type=code&response_mode=query&redirect_uri=https%3A%2F%2Fcgtspa--devmerge.sandbox.my.site.com%2FCGTPortaleRegistrazioneClienti%2Fservices%2Fauthcallback%2FAzureADB2CTest&state=fdfsf';
 
