6fa43be911eaac7ca6c95e81a956d53077c9ddcc
CMELE <c.mele@balanceconsulting.it>
2025-05-05 12:56:38 +0200

    Prod

---
+++ Prod/cgt_conn_sfdc_const_s.js
@@ -0,0 +1,27 @@
+/**************************************************************************************                                                                             
+                                                                             
+                                                                                                                                                       
+           _                                                    __    _      
+          | |                                                  / _|  | |     
+  ___ __ _| |_             ___ ___  _ __  _ __             ___| |_ __| | ___ 
+ / __/ _` | __|           / __/ _ \| '_ \| '_ \           / __|  _/ _` |/ __|
+| (_| (_| | |_           | (_| (_) | | | | | | |          \__ \ || (_| | (__ 
+ \___\__, |\__|           \___\___/|_| |_|_| |_|          |___/_| \__,_|\___|
+      __/ |      ______                           ______                     
+     |___/      |______|                         |______|                    
+
+
+                    Created By Balance Spa
+                    cgt_conn_sfdc_const_s
+                    versione 1.5
+                    18/04/2024
+**********************************************************************************************/
+
+
+const commUrl = 'https://cgtspa.my.site.com/CGTPortaleRegistrazioneClienti';
+const redirectURI = 'https://cridolby.github.io/balanceCGT/PROD';
+const azureLogoutURI = 'https://cgtb2cprod.b2clogin.com/cgtb2cprod.onmicrosoft.com/B2C_1A_SFIdentity_SignUp_SignIn_v2/oauth2/v2.0/logout';
+const clientId = '3MVG9ux34Ig8G5eoTx3FAdE9pADMt2c9avwSgMYEYKmPWap3WbYkrPaXdgfXVYi7SSxPYfJqOe3lhKx3k4qDg';
+const ssoProvider = 'AzureADB2C';
+//const sorgente = 'portaleCGT';
+const sorgente = 'sitoCGT';
